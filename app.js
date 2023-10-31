2f911611eeae412cc2126d92a8850df690d774f3
Anaelle Donfack <anaellejeagni@gmail.com>
2023-10-31 15:10:50 +0100

    Create our first uri :List all stuff and allow CORS

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+//create our  express app
+const app = express();
+
+// Configure CORS access
+app.use((request, response, next)=>{
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+})
+
+app.use('/api/stuff', (request, response, next) => {
+    const stuff = [
+      {
+        _id: 'oeihfzeoi',
+        title: 'Mon premier objet',
+        description: 'Les infos de mon premier objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 4900,
+        userId: 'qsomihvqios',
+      },
+      {
+        _id: 'oeihfzeomoihi',
+        title: 'Mon deuxième objet',
+        description: 'Les infos de mon deuxième objet',
+        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+        price: 2900,
+        userId: 'qsomihvqios',
+      },
+    ];
+    response.status(200).json(stuff);
+  });
+//export our app to access it in our other node files
+module.exports = app;
